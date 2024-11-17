a92edb0bb65561791b5c1165f0ac2d297b103168
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-11-17 11:22:33 +0530

    Create 2 players, play now logs gameboard clone

--- js/script.js
+++ js/script.js
@@ -2,21 +2,24 @@ const Gameboard = (function() {
   const gameboard = [];
   const add = (move) => {
     gameboard.push(move);
-  }
+  };
   const getBoard = () => {
     return [...gameboard];
-  }
+  };
   return { add, getBoard };
 })();
 
 function createPlayer(playerName, playerToken) {
   const name = playerName;
   const token = playerToken;
-  const moves = [];
+  const moves = [];//stores the indices where player has played
   const play = () => {
-    Gameboard.add(token)
-  }
-  return { play }
+    Gameboard.add(token);
+    console.log(Gameboard.getBoard);
+  };
+  return { play };
 }
 
+const player1 = createPlayer("P1", "X");
+const player2 = createPlayer("P2", "O");
 
